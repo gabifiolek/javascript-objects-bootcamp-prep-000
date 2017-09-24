a7603aa7792b0e9158e24585518b826c0edad7a7
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-09-24 13:19:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistNam]e;
+  delete playlist[artistName];
   return playlist;
 }
