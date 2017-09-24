d952d79222fe65c775dc94e9cd243658716c0fd1
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-09-24 09:52:27 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {
+  songTitle: 'Let It Go',
+  artistName: 'John Legend'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] =
+}
+
+function removeFromPlaylist(playlist, artistName) {
+
+}
